49f3501f623dda003c3350db4730154f6f399c5a
Daniel <dan.jordon@gmail.com>
2019-08-22 23:44:20 -0400

    Switched everything up

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,11 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
 
-sh -c "echo Hello world my name is $MY_NAME"
+APP_VERSION=$(cat mix.exs \
+    | grep --line-buffer "version: " \
+    | grep --extended-regexp --only-matching "[0-9\.]+")
+
+echo "The version is supposed to be $APP_VERSION"
+
+APP_FULL_VERSION=$(git describe --dirty --tags)
+
+echo
