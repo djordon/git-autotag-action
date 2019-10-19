609b6cca3e0e20826edd4e979710f5d366d91303
Daniel <dan.jordon@gmail.com>
2019-10-19 15:40:32 -0400

    Make the version 0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Devoro.Mixfile do
   def project do
     [
       app: :devoro,
-      version: "0.18.2",
+      version: "0.1.0",
       elixir: "~> 1.8.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
