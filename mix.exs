ac84d59118729b4524416d25bd04c6a6e71c8988
Daniel Jordon <djordon@users.noreply.github.com>
2019-10-19 15:42:36 -0400

    Clean up repo (#6)
    * Move files to the root of the repository
    * Use master as the action
    * Make the version 0.1.0

--- mix.exs
+++ mix.exs
@@ -1,10 +1,10 @@
-defmodule Devoro.Mixfile do
+defmodule Tags.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app: :devoro,
-      version: "0.18.2",
+      app: :tags,
+      version: "0.1.0",
       elixir: "~> 1.8.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
