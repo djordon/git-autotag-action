bb28db16229c30deaeb26a99b97b91cddf6e369c
Daniel <dan.jordon@gmail.com>
2019-10-19 15:41:55 -0400

    Change the name of the project to tags

--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule Devoro.Mixfile do
+defmodule Tags.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app: :devoro,
+      app: :tags,
       version: "0.1.0",
       elixir: "~> 1.8.1",
       start_permanent: Mix.env() == :prod,
