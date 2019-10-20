02efe902a7c7b6439ce2b457f641e3acbf04db9e
Daniel <dan.jordon@gmail.com>
2019-10-20 01:13:34 -0400

    Simplify the mix config

--- mix.exs
+++ mix.exs
@@ -4,10 +4,7 @@ defmodule Tags.Mixfile do
   def project do
     [
       app: :tags,
-      version: "0.1.0",
-      elixir: "~> 1.8.1",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      version: "0.2.0"
     ]
   end
 end
