7cda9dafd2c6431eb15d8f6c52b802f6e7594072
djordon <dan.jordon@gmail.com>
2021-04-06 00:34:20 -0400

    Make this more portable... for no reason in particular

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ DEFAULT_VERSION='cat VERSION'
 VERSION=$(eval ${VERSION_COMMAND:-$DEFAULT_VERSION} 2>/dev/null)
 V="v"
 
-if [[ ${PREPEND_V} == "false" ]];
+if [ "${PREPEND_V}" = "false" ];
 then
     V=""
 fi
 
-if [ -z $VERSION ]
+if [ -z "$VERSION" ]
 then
     echo "VERSION_COMMAND yielded an empty version. That's probably unexpected. Exiting with status 1."
     exit 1
@@ -18,7 +18,7 @@ fi
 
 TAG=$(git tag | grep --extended-regexp "^${V}${VERSION}$")
 
-if [ ! -z $TAG ]
+if [ ! -z "$TAG" ]
 then
     echo "Tag $TAG already exists. Exiting"
     exit 0
