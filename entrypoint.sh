dcfe7a959dd204ddca28f66d8d52b1960cd93a03
Daniel Jordon <djordon@users.noreply.github.com>
2023-07-15 01:41:04 -0400

    feat: make into a proper action (#17)
    * Add an actions.yml
    * Take only one variable as input
    * Update the shell file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
-# Taken from https://github.com/actions/checkout/issues/760
+if [ -z "$1" ]
+then
+  echo "Must supply the version command. Exiting with status 1."
+  exit 1
+fi
+
+Taken from https://github.com/actions/checkout/issues/760
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 git config --global --add safe.directory /github/workspace
 
-DEFAULT_VERSION='cat VERSION'
-
-VERSION=$(eval ${VERSION_COMMAND:-$DEFAULT_VERSION} 2>/dev/null)
+VERSION=$(eval echo "$1" 2>/dev/null)
 
 if [ -z "$VERSION" ]
 then
-    echo "VERSION_COMMAND yielded an empty version. That's probably unexpected. Exiting with status 1."
+    echo "version-command yielded an empty version. That's probably unexpected. Exiting with status 1."
     exit 1
 fi
 
-TAG=$(git tag | grep --extended-regexp "^${VERSION_PREFIX}${VERSION}$")
+TAG=$(git tag | grep --extended-regexp "^${VERSION}$")
 
 if [ ! -z "$TAG" ]
 then
@@ -24,14 +28,14 @@ fi
 
 COMMIT=$(git rev-parse HEAD)
 
-echo "Tagging the repo with ${VERSION_PREFIX}${VERSION}"
+echo "Tagging the repo with ${VERSION}"
 
 curl --silent --show-error -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/git/refs \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token $GITHUB_TOKEN" \
   -d @- << EOF
 {
-  "ref": "refs/tags/${VERSION_PREFIX}${VERSION}",
+  "ref": "refs/tags/${VERSION}",
   "sha": "${COMMIT}"
 }
 EOF
