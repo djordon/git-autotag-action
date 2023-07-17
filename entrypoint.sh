6d47f68a0aaafa112696091df790c0db3f9094b0
djordon <dan.jordon@gmail.com>
2023-07-17 00:38:25 -0400

    Simplify even further

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,19 @@
 # Using /usr/bin/env bash is probably unnecessary but doesn't hurt.
 # See https://tldp.org/LDP/abs/html/sha-bang.html#AEN269
 
+VERSION="${1:?Must supply the version. Exiting with status 1.}"
+
 # Taken from https://github.com/actions/checkout/issues/760
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 git config --global --add safe.directory /github/workspace
 
-VERSION="${1:?Must supply the version. Exiting with status 1.}"
-
-# grep returns error exit status if there is no match, but that is not actually
-# an "error" here
-TAG=$(git tag | grep --extended-regexp "^${VERSION}$" || true)
-
-if [ ! -z "$TAG" ]
+if [ $(git tag --list "$VERSION") ]
 then
-    echo "Tag $TAG already exists. Exiting"
+    echo "Tag $VERSION already exists. Exiting"
     exit 0
 fi
 
-COMMIT=$(git rev-parse HEAD)
-
-echo "Tagging the repo with ${VERSION}"
+echo "Tagging the repo with ${VERSION} to commit ${GITHUB_SHA}"
 
 curl --silent --show-error --fail-with-body -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/git/refs \
   -H "Accept: application/vnd.github.v3+json" \
@@ -28,6 +22,6 @@ curl --silent --show-error --fail-with-body -X POST https://api.github.com/repos
   -d @- << EOF
 {
   "ref": "refs/tags/${VERSION}",
-  "sha": "${COMMIT}"
+  "sha": "${GITHUB_SHA}"
 }
 EOF
