91b08bebf0580fe18ad06905f4ebf546e71e4af7
djordon <dan.jordon@gmail.com>
2023-07-17 01:43:24 -0400

    Okay, this is better

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,23 @@
 # Using /usr/bin/env bash is probably unnecessary but doesn't hurt.
 # See https://tldp.org/LDP/abs/html/sha-bang.html#AEN269
 
-VERSION="${1:?Must supply the version. Exiting with status 1.}"
+TAG="${1:?Must supply the tag. Exiting with status 1.}"
 
 # Taken from https://github.com/actions/checkout/issues/760
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 git config --global --add safe.directory /github/workspace
 
-if [ $(git tag --list "$VERSION") ]
+# We do not want empty strings as the tag, but this is already checked for
+# above when we define TAG.
+if [ $(git tag --list "$TAG") ]
 then
-    echo "Tag $VERSION already exists. Exiting"
+    echo "Tag $TAG already exists. Exiting"
     exit 0
 fi
 
-echo "Tagging the repo with ${VERSION} to commit ${GITHUB_SHA:?No sha supplied.}"
+echo "Tagging the repo with ${TAG} to commit ${GITHUB_SHA:-$(git rev-parse HEAD)}"
 
-git tag ${VERSION}
-git push origin ${VERSION}
+# Create a lightweight tag
+# See https://git-scm.com/book/en/v2/Git-Basics-Tagging for more
+git tag ${TAG}
+git push origin ${TAG}
